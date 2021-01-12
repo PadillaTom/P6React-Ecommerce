3cd1e76c6daca11b4d120c58ad31603199b1de84
Tomas Padilla <padillatomasagustin@gmail.com>
2021-01-12 09:57:54 +0100

    Filtered CONTEXT

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+import React, { useEffect, useContext, useReducer } from 'react';
+import reducer from '../reducers/filter_reducer';
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -9,21 +9,34 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
+} from '../actions';
+import { useProductsContext } from './products_context';
 
-const initialState = {}
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+};
 
-const FilterContext = React.createContext()
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  // Products Context to get Data:
+  const { products } = useProductsContext();
+  // State for Filter Context
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Getting STUFF from PRODUCTS CONTEXT into this context, to be used:
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
   return (
     <FilterContext.Provider value='filter context'>
       {children}
     </FilterContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+  return useContext(FilterContext);
+};
