db045bbd498b4728b158b7e7ae52a8a0b6fc5047
Tomas Padilla <padillatomasagustin@gmail.com>
2021-01-14 08:56:22 +0100

    List View DONE

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,8 +5,21 @@ import ListView from './ListView';
 
 const ProductList = () => {
   // Context:
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
 
+  // No Array YET:
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>
+        Sorry, No products match your search
+      </h5>
+    );
+  }
+  // LIST View:
+  if (grid_view === false) {
+    return <ListView products={products}></ListView>;
+  }
+  // GRID View:
   return <GridView products={products}>product list</GridView>;
 };
 
